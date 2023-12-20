de4ad571a4eb25025653cd816cf004cb2a188b82
Genevieve Leanne Dominguez <genevieveleanned25@gmail.com>
2023-12-19 22:44:07 -0500

    Deleted code for rectangle

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+//Constant variables 
 const canvas = document.querySelector("#canvas-one");
 const context = canvas.getContext("2d");
 
+//Ensures the canvas covers the entire browser window
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.fillStyle = "white";
-context.fillRect(10, 20, 150, 50);
 
+//Prevent rectangle from being stretched if the browser window is resized
 function adjustRect(event) {
     event.preventDefault();
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    
-    context.fillStyle = "white";
-    context.fillRect(10, 20, 150, 50);
+  
 }
 
 window.addEventListener("resize", adjustRect);
